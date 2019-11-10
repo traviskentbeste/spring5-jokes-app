a2c44c157f1443e2b2113fafb2871e49236c24c3
Travis Kent Beste <travis@tencorners.com>
2019-11-09 22:37:51 -0600

    added in service to controller. closes #6 closes #1

--- src/main/java/com/tencorners/spring5jokesapp/controllers/JokesController.java
+++ src/main/java/com/tencorners/spring5jokesapp/controllers/JokesController.java
@@ -1,5 +1,7 @@
 package com.tencorners.spring5jokesapp.controllers;
 
+import com.tencorners.spring5jokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JokesController {
 
-    @RequestMapping({"/", ""})
-    public String getJokes(Model model) {
+    private JokeService jokeService;
 
-        model.addAttribute("joke", "joke");
+    @Autowired
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJokes(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
 
         return "chucknorris";
     }
