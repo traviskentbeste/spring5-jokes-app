6cb26948b5a0291fb7e63912d7f477326772df32
Travis Kent Beste <travis@tencorners.com>
2019-11-09 21:25:34 -0600

    added context root. closes #5

--- src/main/java/com/tencorners/spring5jokesapp/controllers/JokesController.java
+++ src/main/java/com/tencorners/spring5jokesapp/controllers/JokesController.java
@@ -1,11 +1,13 @@
 package com.tencorners.spring5jokesapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokesController {
 
+    @RequestMapping({"/", ""})
     public String getJokes() {
-        return null;
+        return "Hello world";
     }
 }
