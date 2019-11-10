906138271fd73bca745c931d06a5a4494c8f6fb8
Travis Kent Beste <travis@tencorners.com>
2019-11-09 22:32:16 -0600

    create the service and impl.  closes #3

--- src/main/java/com/tencorners/spring5jokesapp/controllers/JokesController.java
+++ src/main/java/com/tencorners/spring5jokesapp/controllers/JokesController.java
@@ -1,13 +1,17 @@
 package com.tencorners.spring5jokesapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokesController {
 
     @RequestMapping({"/", ""})
-    public String getJokes() {
+    public String getJokes(Model model) {
+
+        model.addAttribute("joke", "joke");
+
         return "chucknorris";
     }
 }
---
+++ src/main/java/com/tencorners/spring5jokesapp/services/JokeService.java
@@ -0,0 +1,7 @@
+package com.tencorners.spring5jokesapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+
+}
